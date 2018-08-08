0c71369b7cbddd9ac7f7f0c14c19156cc746359d
Sally O'Malley <somalley@redhat.com>
2018-08-08 15:32:20 -0400

    add terraform-alpine

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scripts-images
+This is a collection of my various scripts, Dockerfiles, etc., mostly in the case my laptop goes fubar.
--- buildah-from-alpine/terraform-alpine.sh
+++ buildah-from-alpine/terraform-alpine.sh
@@ -5,15 +5,15 @@ cleanup() {
   rm -fv terraform terraform_0.11.7_linux_amd64.zip
 }
 trap "cleanup" EXIT
-ctr=$(buildah from scratch) || (echo "Must be root to execute buildah" && exit 1)
+ctr=$(buildah from alpine) || (echo "Must be root to execute buildah" && exit 1)
 mnt=`buildah mount $ctr` || (echo "Must be root to execute buildah" && exit 1)
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
 cp $(pwd)/terraform $mnt || exit 1
 buildah unmount $ctr
 buildah config $ctr
-buildah commit $ctr terraform-scratch:v0.11.7
+buildah commit $ctr terraform-alpine:v0.11.7
 set +x
-echo "run 'podman run -v "$PWD":"$PWD":ro -v /tmp:/tmp:rw terraform-scratch:v0.11.7 fmt -list -check -write=false' to terraform fmt source"
+echo "run 'podman run -v "$PWD":"$PWD":ro -v /tmp:/tmp:rw terraform-alpine:v0.11.7 /terraform fmt -list -check -write=false' to terraform fmt source"
 echo "to cleanup run 'podman stop [containerid from podman run]; podman rm [containerid from podman run]'"
-echo "to push to docker-daemon run 'buildah push [imageID] docker-daemon:terraform-scratch:v0.11.7'"
+echo "to push to docker-daemon run 'buildah push [imageID] docker-daemon:terraform-alpine:v0.11.7'"
