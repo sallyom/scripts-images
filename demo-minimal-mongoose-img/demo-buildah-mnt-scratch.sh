8d7a1f23f8cd55180750976312efffd4a857e812
Sally O'Malley <somalley@redhat.com>
2018-08-09 13:17:29 -0400

    add demo-buildah-scratch-mongoose

---
+++ demo-minimal-mongoose-img/demo-buildah-mnt-scratch.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+set -x
+
+ctr=$(buildah from scratch)
+mnt=`buildah mount $ctr`
+dnf install -y --installroot=$mnt mongoose --releasever=27 --disablerepo=* --enablerepo=fedora --enablerepo=updates
+dnf clean all --installroot=$mnt
+cp index.html $mnt
+buildah unmount $ctr
+buildah config --entrypoint=/usr/bin/mongoose $ctr
+buildah commit $ctr mongoose-test
+contID=`podman run -td mongoose-test`
+ipAddress=`podman inspect $contID | awk -F  ":" '/IPAddress\"/ {print $2}' | sed 's/"//g' | sed 's/,//g'`
+curl $ipAddress:8080
+set +x
+echo "run 'podman exec -it $contID /bin/bash' to see what's in this container"
+echo "to cleanup run 'podman stop $contID; podman rm $contID'"
