f3c4c4742c8db6a314bcd3c9b126d9a77261ea49
W. Trevor King <wking@tremily.us>
2018-08-16 11:05:41 -0700

    terraform-from-scratch: Add a /tmp dir
    Avoiding:
    
      # podman run terraform-scratch:v0.11.7 --help
      Couldn't setup logging tempfile: open /tmp/terraform-log820361243: no such file or directory

--- buildah-from-scratch/terraform-from-scratch.sh
+++ buildah-from-scratch/terraform-from-scratch.sh
@@ -10,10 +10,11 @@ mnt=`buildah mount $ctr` || (echo "Must be root to execute buildah" && exit 1)
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
 cp terraform $mnt || exit 1
+mkdir $mnt/tmp || exit 1
 buildah unmount $ctr
 buildah config --entrypoint='["/terraform"]' "${ctr}"
 buildah commit $ctr terraform-scratch:v0.11.7
 set +x
-echo "run 'podman run -v "$PWD":"$PWD":ro -v /tmp:/tmp:rw terraform-scratch:v0.11.7 fmt -list -check -write=false' to terraform fmt source"
+echo "run 'podman run -v "$PWD":"$PWD":ro terraform-scratch:v0.11.7 fmt -list -check -write=false' to terraform fmt source"
 echo "to cleanup run 'podman stop [containerid from podman run]; podman rm [containerid from podman run]'"
 echo "to push to docker-daemon run 'buildah push [imageID] docker-daemon:terraform-scratch:v0.11.7'"
