b4d3d93e169da382437bbfd69c1276d3b4b64ac1
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-05-14 10:39:24 -0700

    update scripts to use terraform version 0.12-rc.1

--- buildah-from-alpine/terraform-alpine.sh
+++ buildah-from-alpine/terraform-alpine.sh
@@ -2,17 +2,17 @@
 set -x
 
 cleanup() {
-  rm -fv terraform terraform_0.11.8_linux_amd64.zip
+  rm -fv terraform terraform_0.12.0-rc1_linux_amd64.zip
 }
 trap "cleanup" EXIT
 ctr=$(buildah from alpine) || (echo "Must be root to execute buildah" && exit 1)
 mnt=`buildah mount $ctr` || (echo "Must be root to execute buildah" && exit 1)
-wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
-unzip terraform_0.11.8_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.0-rc1/terraform_0.12.0-rc1_linux_amd64.zip
+unzip terraform_0.12.0-rc1_linux_amd64.zip
 cp terraform "${mnt}/usr/bin" || exit 1
 buildah unmount $ctr
 buildah config $ctr
-buildah commit $ctr terraform-alpine:v0.11.8
+buildah commit $ctr terraform-alpine:v0.12.0-rc1
 set +x
 echo "to cleanup run 'podman stop [containerid from podman run]; podman rm [containerid from podman run]'"
-echo "to push to docker-daemon run 'buildah push [imageID] docker-daemon:terraform-alpine:v0.11.8'"
+echo "to push to docker-daemon run 'buildah push [imageID] docker-daemon:terraform-alpine:v0.12.0-rc1'"
--- buildah-from-scratch/terraform-from-scratch.sh
+++ buildah-from-scratch/terraform-from-scratch.sh
@@ -2,18 +2,18 @@
 set -x
 
 cleanup() {
-  rm -fv terraform terraform_0.11.7_linux_amd64.zip
+  rm -fv terraform terraform_0.12.0-rc1_linux_amd64.zip
 }
 trap "cleanup" EXIT
 ctr=$(buildah from scratch) || (echo "Must be root to execute buildah" && exit 1)
 mnt=`buildah mount $ctr` || (echo "Must be root to execute buildah" && exit 1)
-wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
-unzip terraform_0.11.8_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.0-rc1/terraform_0.12.0-rc1_linux_amd64.zip
+unzip terraform_0.12.0-rc1_linux_amd64.zip
 cp terraform $mnt || exit 1
 mkdir $mnt/tmp || exit 1
 buildah unmount $ctr
 buildah config --entrypoint='["/terraform"]' "${ctr}"
-buildah commit $ctr terraform-scratch:v0.11.8
+buildah commit $ctr terraform-scratch:v0.12.0-rc1
 set +x
-echo "run 'podman run -v "$PWD":"$PWD":ro terraform-scratch:v0.11.8 fmt -list -check -write=false' to terraform fmt source"
+echo "run 'podman run -v "$PWD":"$PWD":ro terraform-scratch:v0.12.0-rc1 fmt -list -check -write=false' to terraform fmt source"
 echo "to cleanup run 'podman stop [containerid from podman run]; podman rm [containerid from podman run]'"
